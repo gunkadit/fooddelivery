2986f447933400b326d259eb7416fee5f7a3b091
gunkadit <adit@iuvencamedia.com>
2015-01-15 15:16:38 +0800

    project food
    Signed-off-by: gunkadit <adit@iuvencamedia.com>

---
+++ fooddelivery/restaurants/Listcity_ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Listcity_ViewController.swift
+//  fooddelivery
+//
+//  Created by I Gusti Ngurah Aditya Dharma on 1/15/15.
+//  Copyright (c) 2015 I Gusti Ngurah Aditya Dharma. All rights reserved.
+//
+
+import UIKit
+
+class Listcity_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.5)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ fooddelivery/restaurants/Selectlocation_ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  Selectlocation_ViewController.swift
+//  fooddelivery
+//
+//  Created by I Gusti Ngurah Aditya Dharma on 1/14/15.
+//  Copyright (c) 2015 I Gusti Ngurah Aditya Dharma. All rights reserved.
+//
+
+import UIKit
+
+class Selectlocation_ViewController: UIViewController {
+
+    @IBOutlet var selectCity: UIButton!
+    @IBOutlet var selectCountry: UIButton!
+    @IBOutlet var selectArea: UIButton!
+    @IBOutlet var searchLocation: UIButton!
+    @IBAction func showRestaurants(sender: AnyObject) {
+    }
+    @IBAction func showCity(sender: AnyObject) {
+        println("CITY")
+        self.performSegueWithIdentifier("showCity", sender: self)
+    }
+    
+    @IBAction func showArea(sender: AnyObject) {
+        println("Area")
+    }
+    @IBAction func showCountry(sender: AnyObject) {
+        println("Country")
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.searchLocation.layer.borderColor = UIColor.whiteColor().CGColor
+        self.searchLocation.layer.borderWidth = 2
+        
+        self.selectCity.addTarget(self, action: "touchDown:", forControlEvents: UIControlEvents.TouchDown)
+        self.selectCity.addTarget(self, action: "touchUp:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.selectCity.addTarget(self, action: "touchUp:", forControlEvents: UIControlEvents.TouchUpOutside)
+        
+        self.selectArea.addTarget(self, action: "touchDown:", forControlEvents: UIControlEvents.TouchDown)
+        self.selectArea.addTarget(self, action: "touchUp:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.selectArea.addTarget(self, action: "touchUp:", forControlEvents: UIControlEvents.TouchUpOutside)
+        
+        
+    }
+    
+    func touchDown(sender: UIButton)
+    {
+        sender.backgroundColor = UIColor.lightGrayColor()
+    }
+    
+    func touchUp(sender: UIButton)
+    {
+        sender.backgroundColor = UIColor.whiteColor()
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let Device = UIDevice.currentDevice()
+        
+        let iosVersion = NSString(string: Device.systemVersion).doubleValue
+        
+        let iOS8 = iosVersion >= 8
+        let iOS7 = iosVersion >= 7 && iosVersion < 8 //check version
+        if segue.identifier == "showCity"
+        {
+            if (iOS8){
+                
+                //Leave this blank if you have set presentation style to "over current context"
+                
+                
+            }
+            else
+            {
+                self.navigationController?.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
+                self.modalPresentationStyle = UIModalPresentationStyle.OverCurrentContext
+            }
+        }
+    }
+    
+   
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
